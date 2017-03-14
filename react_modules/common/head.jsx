84affc86e51ed462eba7e4f1ae14152d409b9f3b
taoqun <793264946@qq.com>
2017-03-14 17:58:22 +0800

    add react

--- react_modules/common/head.jsx
+++ react_modules/common/head.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-
+import { DatePicker, message } from 'antd';
+import "antd/dist/antd.css"
 class Component1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          date: '',
+        };
+    }
+    handleChange(date) {
+        message.info('您选择的日期是: ' + date.toString());
+        this.setState({ date });
+    }
     render() {
         return (
-            <div>Hello World!</div>
-        )
+          <div style={{ width: 400, margin: '100px auto' }}>
+            <DatePicker onChange={value => this.handleChange(value)} />
+            <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
+          </div>
+        );
     }
 }
 
-//导出组件
+// 导出组件
 export default Component1;
--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ var JS = path.resolve(__dirname, 'public/js')
 var REACTMODULES = path.resolve(__dirname,'react_modules')
 var ROOT = path.resolve(__dirname)
 var entryObj = {
-    // "account/login/index": './src/account/login/index.js',
-    // "account/register/index": './src/account/register/index.js',
-    // "birth/index": './src/index/index.js',
-    // "task/index": "./src/task/index.js",
-    // "account/user_info/index": "./src/account/user_info/index.js",
-    // "markdown/index": "./src/markdown/index.js",
+    "account/login/index": './src/account/login/index.js',
+    "account/register/index": './src/account/register/index.js',
+    "birth/index": './src/index/index.js',
+    "task/index": "./src/task/index.js",
+    "account/user_info/index": "./src/account/user_info/index.js",
+    "markdown/index": "./src/markdown/index.js",
     "test/index":"./src/test/index.js",
 }
 module.exports = {
@@ -45,7 +45,7 @@ module.exports = {
         plugins: ['transform-runtime']
     },
     resolve: {
-        extensions: ['', '.js', '.vue'],
+        extensions: ['', '.js', '.vue','.jsx','.css'],
         alias: {
             'vue$': 'vue/dist/vue.js',
             VUEMODULES: VUEMODULES,
@@ -61,7 +61,8 @@ module.exports = {
         inline: true,
         grogress: true,
     },
-    devtool: '#eval-source-map'
+    // 开启以后文件会变大，上线时 关闭
+    // devtool: '#eval-source-map'
 };
 
 if (process.env.NODE_ENV === 'production') {
